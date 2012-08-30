e6f3b2d10106ccbb545ee2721791d52b77e51f35
Greg Hendershott <greghendershott@gmail.com>
2012-08-30 16:50:03 -0400

    Update info.rkt for Glacier in prep for v1.3. Closes #2.

--- info.rkt
+++ info.rkt
@@ -3,13 +3,13 @@
 (define name "aws")
 (define categories '(net))
 (define blurb
-  '("Provides support for some Amazon Web Services,"
-    "such as S3, SDB, SES, SNS, and SQS."))
+  '("Provides support for Amazon Web Services, including"
+    "S3, SDB, SES, SNS, SQS, CloudWatch, Glacier."))
 (define homepage "https://github.com/greghendershott/aws")
 
 (define release-notes
-  '((p "Fix formatting of code examples.")))
-(define version "2012-08-18")
+  '((p "Add support for the brand-new Amazon Glacier service.")))
+(define version "2012-08-30")
 (define can-be-loaded-with 'all)
 
 (define primary-file '("main.rkt"))
