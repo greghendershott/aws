58c4d9c4c47eba99e9cfbcd2c9770a02ac9b9b89
Greg Hendershott <greghendershott@gmail.com>
2012-10-25 10:21:02 -0400

    Fix --redirect-main URI.

--- make-doc.sh
+++ make-doc.sh
@@ -1,1 +1,1 @@
-scribble ++xref-in setup/xref load-collections-xref --redirect-main "http://pre.plt-scheme.org/docs/html/" --dest /tmp/aws-doc/ manual.scrbl
+scribble ++xref-in setup/xref load-collections-xref --redirect-main "http://docs.racket-lang.org/" --dest /tmp/aws-doc/ manual.scrbl
