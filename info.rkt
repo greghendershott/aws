eb5942c1fa669cb4487fc08715f3ed0456d6be58
Greg Hendershott <greghendershott@gmail.com>
2012-11-27 10:57:03 -0500

    Update info.rkt for version 1.4

--- info.rkt
+++ info.rkt
@@ -4,12 +4,12 @@
 (define categories '(net))
 (define blurb
   '("Provides support for Amazon Web Services, including"
-    "S3, SDB, SES, SNS, SQS, CloudWatch, Glacier."))
+    " S3, SDB, SES, SNS, SQS, CloudWatch, Glacier Dynamo, Route 53."))
 (define homepage "https://github.com/greghendershott/aws")
 
 (define release-notes
-  '((p "Add support for the brand-new Amazon Glacier service.")))
-(define version "2012-08-30")
+  '((p "Add support for Dynamo and Route 53.")))
+(define version "2012-11-27")
 (define can-be-loaded-with 'all)
 
 (define primary-file '("main.rkt"))
