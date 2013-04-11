35280a20476aee863a37144a0a20231c5d2a5e94
Greg Hendershott <greghendershott@gmail.com>
2013-04-11 11:27:29 -0400

    Change info.rkt to one for pkg mgr

--- info.rkt
+++ info.rkt
@@ -1,18 +1,2 @@
 #lang setup/infotab
-
-(define name "aws")
-(define categories '(net))
-(define blurb
-  '("Provides support for Amazon Web Services, including S3, SDB, SES, SNS, SQS, CloudWatch, Glacier Dynamo, Route 53."))
-(define homepage "https://github.com/greghendershott/aws")
-
-(define release-notes
-  '((p "Use Racket's new define-logger.")))
-(define version "2013-04-11")
-(define can-be-loaded-with 'all)
-
-(define primary-file '("main.rkt"))
-(define scribblings '(("manual.scrbl" (multi-page))))
-
-(define required-core-version "5.3.1")
-(define repositories '("4.x"))
+(define deps '("http"))
