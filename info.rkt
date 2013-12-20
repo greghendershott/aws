12254fc5e0e4e698f46951bd293a0e8cd40d5883
Greg Hendershott <greghendershott@gmail.com>
2013-12-20 18:37:34 -0500

    Add missing deps and build-deps. Fixes #25.

--- info.rkt
+++ info.rkt
@@ -1,4 +1,9 @@
 #lang setup/infotab
 (define version "0.1")
 (define collection 'multi)
-(define deps '("http" "sha"))
+(define deps '("base"
+               "http"
+               "sha"))
+(define build-deps '("racket-doc"
+                     "rackunit-lib"
+                     "scribble-lib"))
