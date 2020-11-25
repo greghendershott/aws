a457ef5cbe9b516ae752bf9a0d67fe7503c951da
Greg Hendershott <git@greghendershott.com>
2020-11-25 13:08:21 -0500

    Use GitHub Actions instead of Travis CI

--- info.rkt
+++ info.rkt
@@ -3,8 +3,7 @@
 (define collection 'multi)
 (define deps '(["base" #:version "6.3"]
                ["http" #:version "0.3"]
-               "sha"
-               "rackunit-lib"))
+               "sha"))
 (define build-deps '("at-exp-lib"
                      "racket-doc"
                      "rackunit-lib"
