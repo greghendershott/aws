ddeee9c2152f6aabaa55753fe8f4872088db060d
Chip Castle <chip@chipcastle.com>
2023-07-15 10:20:11 -0500

    Add example code for pre-signed URLs

--- examples/examples.rkt
+++ examples/examples.rkt
@@ -63,6 +63,39 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
+;; Pre-signed url's
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+#|
+
+#lang racket
+
+(require aws)
+(require aws/s3)
+
+; Your aws_access_key_id
+(public-key "akid")
+; Your aws_secret_access_key
+(private-key "secret")
+
+; Digital Ocean Spaces is compatible with AWS
+(s3-host "nyc3.digitaloceanspaces.com")
+(s3-region "nyc3")
+(s3-scheme "https")
+
+; Ensure bucket+path is url-encoded
+(define bucket+path "unixcasts/10.mp4")
+(define method "GET")
+(define expires 900)
+
+(sign-uri bucket+path method expires '())
+; Returns signed url valid for expires duration
+
+|#
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;; SDB
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
